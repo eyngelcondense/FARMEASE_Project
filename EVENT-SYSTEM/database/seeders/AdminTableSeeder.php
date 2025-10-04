c4f41cd26358b47f02c9465a3ed6f0b74694d515
Ryan Paulo Magnaye <magnaye.rp@gmail.com>
2025-10-04 22:07:27 +0800

    other seeders added

--- EVENT-SYSTEM/database/seeders/AdminTableSeeder.php
+++ EVENT-SYSTEM/database/seeders/AdminTableSeeder.php
@@ -4,11 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AdminTableSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        DB::table('admin')->insert([
+            'username' => 'superadmin',
+            'password' => Hash::make('password123'), // It's important to hash passwords
+            'role' => 'superadmin',
+            'linkedId' => 0, // Assuming 0 means no linked ID
+            'createdAt' => now()
+        ]);
     }
 }
